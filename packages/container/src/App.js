ff8068938626f277288f0c00371f4b6b8096db31
GauravKarkera <gauravkarkera18@gmail.com>
2023-11-07 23:23:03 +0530

    rendomized classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,19 +2,25 @@ import React from 'react'
 import MarketingApp from "./components/MarketingApp"
 import Header from "./components/Header"
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 // import { mount } from 'marketing/MarketingApp'
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                {/* <h1>Hi THere!!!!!!!!!!!!!</h1>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    {/* <h1>Hi THere!!!!!!!!!!!!!</h1>
                 <hr /> */}
-                <Header />
-                <MarketingApp />
+                    <Header />
+                    <MarketingApp />
 
-            </div>
-        </BrowserRouter>
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 
 }
