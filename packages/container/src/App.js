cdd6cadc3b9f8c65b06403297ef1297c3c587047
GauravKarkera <gauravkarkera18@gmail.com>
2023-11-09 23:26:22 +0530

    removed console log

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -28,7 +28,6 @@ export default () => {
                         <Route path='/' component={MarketingLazy} />
                     </Switch>
                 </Suspense>
-                {/* <MarketingApp /> */}
 
 
             </BrowserRouter>
